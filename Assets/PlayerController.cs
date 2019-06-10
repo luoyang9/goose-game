cca26e11c77254f09760f361d2b22fa163fcbaf8
Alston Lin <alstonlin812@gmail.com>
2019-06-10 15:08:43 -0400

    Added controls

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public const float GRAVITY = -1;
+    public const float GRAVITY_ACCELERATION = 3;
+    public const float MOVEMENT_ACCELERATION = 5;
+    public const float MAX_MOVEMENT_SPEED = 10;
+    public const float MAX_FALL = 15;
+
     public Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -13,8 +17,34 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
     void FixedUpdate()
     {
+        // Default acceleration (with gravity)
+        Vector2 acceleration = new Vector2(0, -GRAVITY_ACCELERATION);
+        if (Input.GetKey(KeyCode.A))
+        {
+            acceleration += Vector2.left * MOVEMENT_ACCELERATION;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            acceleration += Vector2.right * MOVEMENT_ACCELERATION;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            acceleration += Vector2.up * MOVEMENT_ACCELERATION;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            acceleration += Vector2.down * MOVEMENT_ACCELERATION;
+        }
+        Vector2 velocity = rb.velocity + acceleration;
+        velocity.x = Mathf.Min(Mathf.Max(velocity.x, -MAX_MOVEMENT_SPEED), MAX_MOVEMENT_SPEED);
+        velocity.y = Mathf.Min(Mathf.Max(velocity.y, -MAX_MOVEMENT_SPEED), MAX_FALL);
+        rb.velocity = velocity;
     }
 }
