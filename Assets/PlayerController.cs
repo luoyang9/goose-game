2d4ee5148fdfc4f9ec245150b9e6bfbf1fdc0d88
Alston Lin <alstonlin812@gmail.com>
2019-06-10 16:37:38 -0400

    Finished player controls

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public const float GRAVITY_ACCELERATION = 3;
+    public const float JUMP_VELOCITY = 40;
     public const float MOVEMENT_ACCELERATION = 5;
-    public const float MAX_MOVEMENT_SPEED = 10;
-    public const float MAX_FALL = 15;
+    public const float MAX_MOVEMENT_SPEED = 20;
+    public const float MAX_FALL = 40;
 
     public Rigidbody2D rb;
+
+    private bool onGround = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +22,19 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+    }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts.Length > 0)
+        {
+            onGround = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        onGround = false;
     }
 
     void FixedUpdate()
@@ -34,17 +49,17 @@ public class PlayerController : MonoBehaviour
         {
             acceleration += Vector2.right * MOVEMENT_ACCELERATION;
         }
-        if (Input.GetKey(KeyCode.W))
-        {
-            acceleration += Vector2.up * MOVEMENT_ACCELERATION;
-        }
         if (Input.GetKey(KeyCode.S))
         {
             acceleration += Vector2.down * MOVEMENT_ACCELERATION;
         }
         Vector2 velocity = rb.velocity + acceleration;
         velocity.x = Mathf.Min(Mathf.Max(velocity.x, -MAX_MOVEMENT_SPEED), MAX_MOVEMENT_SPEED);
-        velocity.y = Mathf.Min(Mathf.Max(velocity.y, -MAX_MOVEMENT_SPEED), MAX_FALL);
+        velocity.y = Mathf.Max(velocity.y, -MAX_FALL);
+        if (Input.GetKey(KeyCode.W) && onGround) // Jump
+        {
+            velocity.y = JUMP_VELOCITY;
+        }
         rb.velocity = velocity;
     }
 }
