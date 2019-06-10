e3ca0dc5114a341485b7b2ae4a050b6d4d71a378
Alston Lin <alstonlin812@gmail.com>
2019-06-10 14:35:56 -0400

    Using dynamic rigidbodies

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public const float GRAVITY = -1;
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
