eaddde484d0fa74422bcbeadf7d54620bc922e94
Alston Lin <alstonlin812@gmail.com>
2020-01-14 15:15:14 -0500

    Arrow starts closer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -42,7 +42,7 @@ public class PlayerController : MonoBehaviour {
     public const float FALL_THROUGH_SPEED = 0.5f;
     // Arrows
     public const float ARROW_COOLDOWN = 0.5f;
-    public const float ARROW_START_DIST = 2f;
+    public const float ARROW_START_DIST = 1f;
     public const int START_ARROWS = 5;
     // Melee
     public const float SWING_COOLDOWN = 0.5f;
