acb245348c80b7e1131199a6b134a9f2c0c3a0fd
Alston Lin <alstonlin812@gmail.com>
2020-02-15 17:29:51 -0500

    Destroy GameManager in EndGame

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,6 +8,7 @@ public class EndGame : MonoBehaviour {
         GameManager manager = GameObject.Find("GameManager").GetComponent<GameManager>();
         TMPro.TextMeshProUGUI text = GameObject.Find("Winner").GetComponent<TMPro.TextMeshProUGUI>();
         text.SetText("Player " + (manager.GetWinnerId() + 1) + " won");
+        Destroy(manager.gameObject);
     }
     public void OpenMenu() {
         SceneManager.LoadScene("PlayerSelection");
