984776c98ebecbbd844d5903ed99fb3bb3658bb3
mrdofu <intense_smurf@live.com>
2020-02-26 20:32:45 -0500

    Revert "Hides cursor during game"
    This reverts commit 7fe998c6b27216688691bdd66c2cd4d15521b8e8.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -47,7 +47,6 @@ public class GameManager : MonoBehaviour
 
         if (numPlayers <= 1)
         {
-            Cursor.visible = true;
             SceneManager.LoadScene("EndGame");
         }
     }
