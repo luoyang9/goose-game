16f0c0dcd37b0dbf814042baf304618d2a13176b
Danyal Raza <danyalraza@gmail.com>
2020-03-24 02:09:18 -0400

    Rebase fixes

---
+++ Assets/Scripts/PlayerAfterImagePool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAfterImagePool : MonoBehaviour
+{
+    public GameObject afterImagePrefab;
+
+    public PlayerController attachedPlayer;
+
+    private Queue<GameObject> availableObjects = new Queue<GameObject>();
+
+    private void Awake()
+    {
+    }
+
+
+}
